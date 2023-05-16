c521429b67b43117493e696ebcd047deb0944a97
Ziad7asn <ziad.ehab15@yahoo.com>
2023-05-16 08:15:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st 
 import numpy as np
 import pandas as pd 
-import matplotlib.pyplot as plt 
 import seaborn as sns 
 import plotly.express as px 
 from  streamlit_option_menu import option_menu
